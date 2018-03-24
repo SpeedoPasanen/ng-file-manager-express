7c083efda5a98aa5a0516fe8ad2d79f28c46924c
Toni Lehtinen <tolehtin@gmail.com>
2018-03-24 17:09:16 +0200

    Add mkdirp to deps

--- src/lib/models/ngfm-item.ts
+++ src/lib/models/ngfm-item.ts
@@ -9,6 +9,4 @@ export class NgfmItem {
     lastModified: number;
     created: number;
     readonly itemType: string;
-    get isFile() { return this.itemType === 'file'; }
-    get isFolder() { return this.itemType === 'folder'; }
 }
